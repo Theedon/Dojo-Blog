67261e0b56bc0571390e405c63cb1c4dd59cc6e9
Theedon <faniyantoyin@gmail.com>
2023-06-29 10:20:01 +0100

    code that shows dynamic values and functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const code = (x, y) => {
+    return x * y;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content"></div>
+      <h1>App Component</h1>
+      <div>{code(2, 3)}</div>
     </div>
   );
 }
