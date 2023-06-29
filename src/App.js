1923a61b64e2ab582328c68d16be1d728f13eadb
Theedon <faniyantoyin@gmail.com>
2023-06-29 10:42:16 +0100

    4 components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const code = (x, y) => {
     return x * y;
   };
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const obj = { name: "Yoshida", age: 33 };
+  const link = "https://www.google.com";
   return (
     <div className="App">
       <div className="content"></div>
-      <h1>App Component</h1>
-      <div>{code(2, 3)}</div>
+      <h1>{title}</h1>
+      <p>Liked {likes} times</p>
+      <p>
+        {obj.name} is {obj.age} years old
+      </p>
+      <p>{Math.round(Math.random() * 100) + " is a random number"}</p>
+      <p>
+        This is the use of the function (code) declared earlier: {code(2, 3)}
+      </p>
+      <p>
+        this is an array {"["} {[1, 2, 3, 4]} = bunched together as a string
+        {" ]"}
+      </p>
+      <a href={link}>Link to web search</a>
+      <img src={logo} alt="" />
     </div>
   );
 }
