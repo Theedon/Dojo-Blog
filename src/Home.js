e94c0254d78a30a4877e8706a8f7eb148890f7cc
Theedon <faniyantoyin@gmail.com>
2023-07-02 18:51:35 +0100

    7 click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,36 @@
 const Home = () => {
+  const logToConsole = (variable, e) => {
+    console.log(e);
+    if (variable === "") {
+      console.log("nothing");
+      alert("nothing");
+    } else {
+      console.log(`i am sending ${variable} to the console`);
+      alert(`${variable}`);
+    }
+  };
+  const showEvent = (e) => {
+    console.log(e);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      {/* for the function above, i had to create an anonymous function before i invoked my function and its argument inside of it */}
+      <button
+        onClick={() => {
+          logToConsole("");
+        }}
+      >
+        BUTTON ONE
+      </button>
+      <button
+        onClick={(e) => {
+          logToConsole("tola", e);
+        }}
+      >
+        BUTTON TWO
+      </button>
+      <button onClick={showEvent}>BUTTON 3</button>
     </div>
   );
 };
