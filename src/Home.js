26885f2adc54cd821a6a7b0833cda47f4000d9db
Theedon <faniyantoyin@gmail.com>
2023-07-04 16:23:21 +0100

    17 fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,74 +2,20 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-  let newBlog = {
-    title: "New Blog",
-    body: "This is the content of the new blog added",
-    author: "toyin",
-    id: 1,
-  };
-  const [name, setName] = useState("Mario");
-
-  const addBlog = () => {
-    if (blogs.length >= 1) {
-      newBlog.id = blogs[blogs.length - 1].id + 1;
-      setBlogs([...blogs, newBlog]);
-    } else {
-      newBlog.id = 1;
-      setBlogs([newBlog]);
-    }
-  };
-  const deleteBlog = (id) => {
-    const filteredBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(filteredBlogs);
-  };
+  const [blogs, setBlogs] = useState();
 
   useEffect(() => {
-    console.log("useEffect hook ran");
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
-  return (
-    <div>
-      <BlogList
-        blogs={blogs}
-        title="All Blogs"
-        addBlog={addBlog}
-        deleteBlog={deleteBlog}
-        newBlog={newBlog}
-      />
-      {/* <BlogList
-        blogs={blogs.filter((blog) => blog.author == "mario")}
-        title="Mario's Blogs"
-        newBlog={newBlog}
-        addBlog={addBlog}
-        deleteBlog={deleteBlog}
-      /> */}
-      <div className="container">
-        <p>{name}</p>
-        <button
-          onClick={() => {
-            if (name === "Mario") {
-              setName("Luigi");
-            } else {
-              setName("Mario");
-            }
-          }}
-        >
-          EFFECT
-        </button>
-      </div>
-    </div>
-  );
+  return <div>{blogs && <BlogList blogs={blogs} title="All Blogs" />}</div>;
 };
 
 export default Home;
