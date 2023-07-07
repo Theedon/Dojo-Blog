fb6b38f48cfa1a119aaa6cc40c9ae7b1f6946ba2
Theedon <faniyantoyin@gmail.com>
2023-07-07 13:09:14 +0100

    19 handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,16 +4,24 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((response) => {
+          if (!response.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
           return response.json();
         })
         .then((data) => {
-          console.log(data);
           setBlogs(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
           setIsLoading(false);
         });
     }, 1000);
@@ -21,6 +29,11 @@ const Home = () => {
 
   return (
     <div>
+      {error && (
+        <div className="blog-preview">
+          <p>{error}</p>
+        </div>
+      )}
       {isLoading && (
         <div className="blog-preview">
           <p>page loading...</p>
@@ -34,11 +47,3 @@ const Home = () => {
 export default Home;
 
 // run npx json-server --watch ./data/db.json --port 8000 to start the json server
-
-// const runFunction = async () => {
-//   useEffect(() => {
-//     const response = axios.get("http://localhost:8000/blogs");
-//     const data = response.json(response);
-//     setBlogs(data);
-//   }, [])
-// }
